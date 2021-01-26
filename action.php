1cacd36d49147b0aa766c25c89cd4d5b4a3cc97b
arionfarhi <arionfarhi@gmail.com>
2021-01-26 12:49:18 -0500

    g

--- action.php
+++
@@ -1,9 +0,0 @@
-<?php
- $path = 'data.txt';
- if (isset($_POST['f_name']) && isset($_POST['l_name'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['f_name'].' - '.$_POST['l_name'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
- }
-?>
