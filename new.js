fa53867a97fa8293bffd5e14f8a0d1bff704edbb
arionfarhi <arionfarhi@gmail.com>
2021-03-02 01:30:44 -0500

    hey

--- new.js
+++ new.js
@@ -1,14 +1,14 @@
 
-	var firebaseConfig = 
-		{
-			apiKey: "AIzaSyB3GZydX8zppzuImZfD2eQyY60j85Sq5as",
-			authDomain: "hangrdatabase.firebaseapp.com",
-			projectId: "hangrdatabase",
-			storageBucket: "hangrdatabase.appspot.com",
-			messagingSenderId: "277534831276",
-			appId: "1:277534831276:web:1ecb8a057c0465af1ca509",
-			measurementId: "G-RGQD8RJWTM"
-		};
+var firebaseConfig = 
+{
+	apiKey: "AIzaSyB3GZydX8zppzuImZfD2eQyY60j85Sq5as",
+	authDomain: "hangrdatabase.firebaseapp.com",
+	projectId: "hangrdatabase",
+	storageBucket: "hangrdatabase.appspot.com",
+	messagingSenderId: "277534831276",
+	appId: "1:277534831276:web:1ecb8a057c0465af1ca509",
+	measurementId: "G-RGQD8RJWTM"
+};
 
 		// initialize firebase
 
@@ -26,86 +26,89 @@
 			/* depends on inner html of titleText, make sure works with Georgetown */
 			var college_selection_val = document.getElementById("titleText").innerHTML.substring(78).split("<")[0].toUpperCase();
 
-
-			var course_name_val = (((document.forms["add_group"]["course_name"].value).replace(/ /g, "")));
-			var dash_index = course_name_val.indexOf("-");
-
-			if (course_name_val.substring(dash_index+1).indexOf("-") != -1) //if second dash exists
+			
+			if (college_selection_val.includes("/"))
 			{
-				dash_index = course_name_val.substring(dash_index+1).indexOf("-") + dash_index;
-				course_name_val = course_name_val.replace("-","");
+				college_selection_val = college_selection_val.split("/")[1];
+			}
+
+
+			var course_name_val = (((document.forms["add_group"]["course_name"].value).replace(/ /g, "").replaceAll("-","").toLowerCase()));
+
+			var nums = course_name_val.match(/\d/);
+			var first_num_occurance = course_name_val.indexOf(nums);
+			var last_alphabet_occurance = -1;
+			
+			var last_alphabet_occurance = "";
+			var i = 0;
+			var contains_letters = "false";
+
+			//finds last alphabet occurance
+			for (i=0; i< course_name_val.length; i++)
+			{
+				if (course_name_val[i].match(/^[a-z]+$/))
+				{
+					last_alphabet_occurance = i;
+					contains_letters = "true";
+				}
+			}
+
+
+			//if first number comes before last letter
+			if (first_num_occurance < last_alphabet_occurance)
+			{
+				submission_error.innerHTML = "Please fix formatting of Course Name";
+				return 0; //exit 
 			}
 			
 
-			if ((dash_index > 2) && /\d/.test(course_name_val.substring(dash_index -1, dash_index))) //if previous entry is number, ie not in like "Econ-UB"
+			//if course name has no alphabetic characters
+			if (contains_letters == "false")
 			{
-				course_name_val = course_name_val.replace("-"," ");
-			}
-			else if (dash_index == -1)
-			{
-				//do nothing
-			}
-			else
-			{
-				submission_error.innerHTML = "Course Name is not formatted correctly. Please fix dashes."
-				return 0; //exit
+				submission_error.innerHTML = "Please fix formatting of Course Name";
+				return 0; //exit 
 			}
 
+			//if numbers span over 4 in length
+			if ((course_name_val.substring(last_alphabet_occurance+1)).length > 4)
+			{
+				submission_error.innerHTML = "Please do not include section number";
+				return 0; //exit 
+			}
+
+			
+			//if course name is too long
 			if (course_name_val.length > 20)
 			{
 				submission_error.innerHTML = "Please shorten the Course Name";
 				return 0; //exit 
 			}
 
+			//if course name is too short
 			if (course_name_val.length < 5)
 			{
 				submission_error.innerHTML = "Course Name is too short";
 				return 0; //exit 
 			}
 
- 			if(!(course_name_val.match(/^[0-9a-zA-Z ]+$/)))
- 			{
- 				submission_error.innerHTML = "Course Name must contain only letters, numbers, and a dash";
+
+			// if course contains extraneous characters
+			if(!(course_name_val.match(/^[0-9a-zA-Z ]+$/)))
+			{
+				submission_error.innerHTML = "Course Name may contain only letters, numbers, and dashes";
  				return 0; //exit
  			}
 
- 			if(!(/\d/.test(course_name_val))) //if contains numbers
+ 			//if doesn't contain numbers
+ 			if(!(/\d/.test(course_name_val))) 
  			{
  				submission_error.innerHTML = "Course Name must contain numbers";
  				return 0; //exit
  			}
 
- 			/* Find section number */
- 			try
- 			{
- 				var section_number = course_name_val.split(" ")[1];
- 				section_number = section_number.toString(); 
- 				section_number = section_number.replace(/^0+/, ''); //gets rid of leading zeroes
-
- 				if (section_number.length > 3)
- 				{
- 					submission_error.innerHTML = "Section Number in Course Name cannot be more than 3 digits";
- 					return 0; //exit
- 				}
-
- 				if (section_number.toString().length == 1)
- 				{
- 					section_number = "00" + section_number;
- 				}
-
- 				if (section_number.toString().length == 2)
- 				{
- 					section_number = "0" + section_number;
- 				}
-
- 				course_name_val = course_name_val.split(" ")[0] + " " + section_number;
- 			}
- 			catch (no_section_number)
- 			{
- 			}
 
 
-			var course_nickname_val = ((document.forms["add_group"]["course_nickname"].value).trim().toLowerCase());
+ 			var course_nickname_val = ((document.forms["add_group"]["course_nickname"].value).trim().toLowerCase());
 
 			if (course_nickname_val.length > 42) //will go onto other line otherwise
 			{
@@ -284,29 +287,29 @@
 
 			var data = // how data is held before it is pushed/submitted to entry
 			{
-		 		name: 
-		  	    course_name_val,
+				name: 
+				course_name_val,
 
-		 		nickname:
+				nickname:
 				course_nickname_val,
 
-		  		professor:
-		 		recitation_val + course_professor_val + recitation_placeholder,
+				professor:
+				recitation_val + course_professor_val + recitation_placeholder,
 
-		 		start_time:
-		 		course_start_time_val,
+				start_time:
+				course_start_time_val,
 
-		 		end_time:
-		 		course_end_time_val,
+				end_time:
+				course_end_time_val,
 
-		  		days_of_week:
-		 		course_days_of_week,
+				days_of_week:
+				course_days_of_week,
 
-		 		link:
-		 		course_link_val
-		    }
+				link:
+				course_link_val
+			}
 
-		    ref.push(data);
+			ref.push(data);
 		}
 
 
@@ -321,22 +324,22 @@
 
 			database.ref(collegeName).orderByChild("name").once('value', function(snapshot)
 			{
-       			if(snapshot.exists())
-       		 	{
-          			var content = '';
-          			snapshot.forEach(function(data){
-             		var val = data.val();
+				if(snapshot.exists())
+				{
+					var content = '';
+					snapshot.forEach(function(data){
+						var val = data.val();
 
-             		/* Ensure format of val.professor is correct */
+						/* Ensure format of val.professor is correct */
 
-             		if (val.professor.includes("recitation"))
-             		{
-             			val.professor = val.professor.substring(0,1).toUpperCase() + val.professor.substring(1);
-             		}
-             		else
-             		{
-             			val.professor = val.professor.substring(0,6) + val.professor.substring(6,7).toUpperCase() + val.professor.substring(7);
-             		}
+						if (val.professor.includes("recitation"))
+						{
+							val.professor = val.professor.substring(0,1).toUpperCase() + val.professor.substring(1);
+						}
+						else
+						{
+							val.professor = val.professor.substring(0,6) + val.professor.substring(6,7).toUpperCase() + val.professor.substring(7);
+						}
 
              		//course_name_beginning = val.name.split("-")[0];
 
@@ -367,10 +370,10 @@
              		content += '<a href= "' + val.link + '" >';
 
              		/* alternate colors */
-      				if (i==0)
-      				{
+             		if (i==0)
+             		{
              			content += '<div class= "courseBackground" style= "background-color: rgb(217,151,52)">';
-      				}
+             		}
 
              		if (i==1)
              		{
@@ -410,13 +413,13 @@
 
              		content +='<tr>';
 
-              		content += '<td>' + '<span class= "heading"> ' + 
+             		content += '<td>' + '<span class= "heading"> ' + 
              		val.nickname + " </span> <br> <span class= 'heading2'>" +
-              		val.professor + "</span> <br> <span class= 'info'> " +
-              		val.name + " </span> <br> <span class = 'info2'>" +
-              		val.days_of_week + " | <span class = 'time'> " +
-              		val.start_time + "-" + val.end_time + 
-              		'</span> </div> </a> </span> </td>'
+             		val.professor + "</span> <br> <span class= 'info'> " +
+             		val.name + " </span> <br> <span class = 'info2'>" +
+             		val.days_of_week + " | <span class = 'time'> " +
+             		val.start_time + "-" + val.end_time + 
+             		'</span> </div> </a> </span> </td>'
 
              		content += '</tr>';
 
@@ -427,12 +430,12 @@
              			i= 0;
              		}
 
-           		 });
+             	});
 
-            		$('#table').append(content);
+					$('#table').append(content);
 
-        		}
-    		});
+				}
+			});
 
 		}
 
@@ -445,6 +448,14 @@
 			if (collegeName == "GEORGETOWN")
 			{
 				document.getElementsByTagName("small")[0].style.color = "#" + "00183F";
+			}
+			if (collegeName == "PSU")
+			{
+				document.getElementsByTagName("small")[0].style.color = "#" + "1E407C";
+			}
+			if (collegeName == "PITT")
+			{
+				document.getElementsByTagName("small")[0].style.color = "#" + "FFB81C";
 			}
 		}
 
@@ -495,7 +506,7 @@
 			document.getElementById("push").style.marginLeft = "105%";
 			  //document.getElementById("menuButton").style.display = "none";
 			 // document.getElementById("titleText").style.display = "none";
-		}
+			}
 
 		function closeForm() 
 		{
@@ -508,6 +519,27 @@
 		function goBack()
 		{
 			location.href = "index.html";
+		}
+
+		function search_index()
+		{
+			var input = document.getElementById("search_bar_index").value.toLowerCase();
+			var college_list = document.getElementsByClassName("college");
+			var college_list_length = college_list.length;
+
+			for (var i= 0; i < college_list_length; i++)
+			{
+				if (college_list[i].innerHTML.toLowerCase().includes(input))
+				{
+					$(college_list[i]).show();
+				}
+
+				else
+				{
+					$(college_list[i]).hide();
+				}
+			}
+			
 		}
 
 
