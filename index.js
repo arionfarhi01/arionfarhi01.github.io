79e0c283b61f2c19d0b1a617f0c1b7c3dd39c97a
arionfarhi <arionfarhi@gmail.com>
2021-06-04 01:52:09 -0400

    g

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+/* Begin Modal */
 var modal = document.getElementById("feedback_modal");
 var btn = document.getElementById("contactButton");
 var span = document.getElementsByClassName("closeModal")[0];
@@ -13,18 +14,19 @@ function closeModal()
 	modal.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() 
 {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+window.onclick = function(event) 
+{
+  if (event.target == modal) 
+  {
     modal.style.display = "none";
   }
 }
+/* End Modal */
 
 function goToUpdates()
 {
